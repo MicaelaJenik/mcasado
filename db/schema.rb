b7a1b2807a62915254c8b368e55b0427c596ba63
Micaela Jenik <micaela.jenik@gmail.com>
2022-02-17 16:08:32 -0300

    first migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_02_17_190406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "frontImage"
+    t.string "bioTitle"
+    t.text "bioContent"
+    t.string "picture"
+    t.string "instagram"
+    t.string "web"
+    t.string "cv"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
