2bb79e44efacac4fe55d1e5fb811e70f8282adb3
Micaela Jenik <micaela.jenik@gmail.com>
2022-02-17 16:58:33 -0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  get 'artists/index'
-  get 'artists/show'
   root to: 'pages#home'
+  resources :artists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+  # root to: 'pages#welcome'
+  # get 'home', to: 'pages#home'
+  # get 'press', to: 'pages#press'
+  # get 'contacto', to: 'pages#contacto'
+  # get 'playlist', to: 'pages#playlist'
+  # resources :artworks
+  # resources :shows
+  # resources :staffs
+  # resources :carnivals
