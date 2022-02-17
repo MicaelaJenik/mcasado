9a7c00ce9b1718ed92e086dd82638410d8e4374a
Micaela Jenik <micaela.jenik@gmail.com>
2022-02-17 17:15:57 -0300

    donnne

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,17 @@
 class ArtistsController < ApplicationController
+  before_action :set_artists
+  # has_many :artworks
+
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
+  end
+
+  private
+
+  def set_artists
   end
 end
